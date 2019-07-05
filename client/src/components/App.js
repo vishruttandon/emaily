8c3b7e2aaf17ee40d1f99ace7e79be083e8a851f
v_tandon <v_tandon@something.com>
2019-07-05 23:02:17 +0530

    Users can see commit results

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { BrowserRouter , Route  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+
+
 import Landing from './Landing';
-
-
 import Header from './Header';
-const Dashboard = () => <h2> Dashboard</h2>;
-
-const SurveyNew = () => <h2> SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 
 class App extends Component
@@ -20,16 +19,16 @@ class App extends Component
 	render(){
 		return (
 
-		<div className="container">
+
 			<BrowserRouter>
-				<div> 
+				<div className="container"> 
 					<Header />
 					<Route exact  path="/" component = { Landing }/>
 					<Route exact  path="/surveys" component = { Dashboard }/>
 					<Route path="/surveys/new" component = { SurveyNew }/>
 				</div>
 			</BrowserRouter>
-		</div>
+
 		);
 	}
 	
--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,11 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 
 const store = createStore( reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
 	<Provider store = {store}> <App /> </Provider>,
 	document.querySelector('#root'));
-
-
-console.log("Stripe Key :" ,process.env.REACT_APP_STRIPE_KEY);
-console.log("Environment is  :", process.env.NODE_ENV);
